f262ee8f11aba288bc174c4c9977e19d4be0b50e
Harsh Tyagi <harsh268tyagi@gmail.com>
2025-06-27 03:01:58 +0530

    Adapted the code to log the port application is up. Changed the version of the build Adapted the readme.md and Dockerfile

--- src/main/java/com/assignment/meal/EncryptSecret.java
+++ src/main/java/com/assignment/meal/EncryptSecret.java
@@ -7,7 +7,7 @@ public class EncryptSecret {
         AES256TextEncryptor encryptor = new AES256TextEncryptor();
         encryptor.setPassword("Harsh_PS");
         String encryptedApiId = encryptor.encrypt("c89f7fd0");
-        String encryptedApiKey = encryptor.encrypt("cac18b4a73b04203ef34694419d35b89");
+        String encryptedApiKey = encryptor.encrypt("b18626c81ca340a61e64ebf9ab1d4f49");
         String encryptedAccountUser = encryptor.encrypt("PS");
         String documenuApiKey = encryptor.encrypt("1f7b2c0d8e3a4b6c9f5d8e3a4b6c9f5d");
 
